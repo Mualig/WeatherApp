acac543700e10c03b7eef2bddba408fef7d7097a
Mualig <mualig@gmail.com>
2018-03-20 15:31:37 +0100

    Lesson 37 - Advanced promises

--- playgroung/promise.js
+++ playgroung/promise.js
@@ -1,8 +1,29 @@
+let asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers.');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(1, 2).then((res) => {
+    console.log('Result: ', res);
+    return asyncAdd(res, 33);
+}).then((res) => {
+    console.log('2nd result: ', res);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+});
+
 let somePromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('Hey. It worked!');
         // reject('Unable to fulfill promise.')
-    }, 2500);
+    }, 0);
 });
 
 somePromise.then((message) => {

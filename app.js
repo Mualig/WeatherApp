cecd5d06bc0a27fe3b1e559178d6e5d44c0d9116
Mualig <mualig@gmail.com>
2018-03-20 11:33:35 +0100

    Lesson 32 - Callabck Errors

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import config from './config/config.json';
+
 const request = require('request');
 const yargs = require('yargs');
 
@@ -17,10 +19,19 @@ const argv = yargs
 encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.apiKey}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body.results[0].formatted_address, undefined, 2));
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
+    console.log(body);
+
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find address');
+    } else if (body.status === 'OK') {
+        console.log(JSON.stringify(body.results[0].formatted_address, undefined, 2));
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
